d6916aaa0891fcdef93ad5e191a0272a32ab889e
Kevin Hua <boogerlad@gmail.com>
2015-07-29 18:10:38 -0400

    finalize ui. cool css. have to use jquery :(

--- a.js
+++ a.js
@@ -1,10 +1,24 @@
 function toggleVisibility(element)
 {
-	var possibilities = document.getElementsByClassName('possible');
-	var visibility = element.checked ? 'visible' : 'hidden';
-	for(var i = 0; i < possibilities.length; ++i)
+	if(element.checked)
 	{
-		possibilities[i].style.visibility = visibility;
+		$(".possible").each
+		(
+			function()
+			{
+				$(this).css('background-image', "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10 10'><line x1='1' y1='1' x2='9' y2='9' stroke='red'/><line x1='1' y1='9' x2='9' y2='1' stroke='red'/></svg>\")");
+			}
+		);
+	}
+	else
+	{
+		$(".possible").each
+		(
+			function()
+			{
+				$(this).css('background-image', "none");
+			}
+		);
 	}
 }
 document.getElementById('visibleMoves').onchange = function(){toggleVisibility(this)};
